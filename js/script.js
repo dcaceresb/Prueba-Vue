19419c9e3bc7f0617cfa8ae5886a1cf554d3fd6b
David Cáceres <dcacerbe@outlook.com>
2019-04-11 11:49:41 -0400

    Primer commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+new Vue({
+    el: '#appVue',
+    data:{
+        lista:[
+            {tarea: 'uno', completado: false},
+            {tarea: 'dos', completado: false},
+            {tarea: 'tre', completado: false},
+        ],
+    },
+
+});
